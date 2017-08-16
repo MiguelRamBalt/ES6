362e869c25278ade34b9c6458474cf97542496c8
Miguel Angel Ramirez Baltazar <miguel ramirez@tiempodev.local>
2017-08-16 10:37:22 -0500

    test: adding for testing prupose

--- TemplateStrings/app.js
+++ TemplateStrings/app.js
@@ -10,8 +10,6 @@ Refactor the function to use template strings
 function doubleMessage(number) {
   return `Your number doubled is ${2 * number}`;
 }
-
-
 /*
 
 Refactor the function to use template strings
@@ -19,7 +17,6 @@ Refactor the function to use template strings
   function fullName(firstName, lastName) {
     return firstName + lastName;
   }
-
 */
 function fullName(firstName, lastName) {
   return `${firstName}  ${lastName}`;
